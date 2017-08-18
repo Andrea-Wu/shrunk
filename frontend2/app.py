d5f843ea2f65981249f4547f5c75b1962941ff55
Rui <rz187@scarletmail.rutgers.edu>
2017-08-18 16:57:16 -0400

    made the htmls for flasky

--- frontend2/app.py
+++ frontend2/app.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views", static_folder="statics")
 
 sample_data = [
     {
-        short_url: "19deid",
-        long_url: "asiodfjasdfikjao90wiefjaslodfs.com"
+        'short_url': "19deid",
+        'long_url': "asiodfjasdfikjao90wiefjaslodfs.com"
     },
     {
-        short_url: "io90ko",
-        long_url: "aisd0ofjawkrw0ofjaslkfjwa90f,2309sdzx89va0sf.com"
+        'short_url': "io90ko",
+        'long_url': "aisd0ofjawkrw0ofjaslkfjwa90f,2309sdzx89va0sf.com"
     },
     {
-        short_url: "348eso",
-        long_url: "90kljdf890asefjasdklfsdaf,qw3fp0asfjaseifosdj0ofij.com"
+        'short_url': "348eso",
+        'long_url': "90kljdf890asefjasdklfsdaf,qw3fp0asfjaseifosdj0ofij.com"
     },
     {
-        short_url: "92lslp",
-        long_url: "i89awofnkawlfsjdfo90iasdjfasdl;kfjsd.com"
+        'short_url': "92lslp",
+        'long_url': "i89awofnkawlfsjdfo90iasdjfasdl;kfjsd.com"
     },
     {
-        short_url: "xzcvim",
-        long_url: "9awiofjsdklfasdjf90awesiofjlskdfjsadfsfs.com"
+        'short_url': "xzcvim",
+        'long_url': "9awiofjsdklfasdjf90awesiofjlskdfjsadfsfs.com"
     },
     {
-        short_urL: "",
-        long_url: ""
+        'short_url': "",
+        'long_url': ""
     }
 ]
 
@@ -33,8 +33,6 @@ sample_data = [
 @app.route('/index')
 @app.route('/index.html')
 def root():
-    pass
+    return render_template('admin_dashboard.html')
 
-@app.route('/dashboard')
-def dash():
-    pass
+app.run(debug=True, port=3000)
